f584c15191c6cc36585bb73821b4be91db92ef70
vegardmj <47636167+vegardmj@users.noreply.github.com>
2025-11-04 18:49:43 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT for GitHub Pages under /veins-of-power-app/
-  base: "/veins-of-power-app/",
-  build: { outDir: "docs" }, // publish folder Pages uses
+  base: "/veins-of-power-app/", // <- repo name
+  build: { outDir: "docs" }, // <- emit to docs/
 });
